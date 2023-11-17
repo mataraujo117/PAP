e1f4264000267a094d5346899be9ebe5f04b9f66
Mateus Araujo <mateusaraujo3105@gmail.com>
2023-11-17 21:18:22 +0000

    Initial commit.

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlueBerryBear</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+    <script src="https://kit.fontawesome.com/21ee8a4c9a.js" crossorigin="anonymous" defer></script>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <div class="header">
+        <div class="logo">
+            <img src="./img/bear.png">
+            <div class="logo-name">BlueBerryBear</div>
+        </div>
+        <div class="navbar">
+            <a href="#">Home</a>
+            <a href="#">Moedas</a>
+            <a href="#">Serviços</a>
+            <a href="#">Sobre</a>
+            <a href="#">Contactos</a>
+        </div>
+    </div>
+
+    <!-- section 1 -->
+    <div class="main-area">
+
+        <!-- Texto -->
+        <div class="container-1">
+            <div class="box-1">Invista em Criptomoedas</div>
+            <div class="box-2">Investir em criptomoedas tem se destacado como uma alternativa atrativa e inovadora no mundo financeiro contemporâneo. As criptomoedas, como o Bitcoin e o Ethereum, representam uma mudança significativa no paradigma tradicional de investimentos.</div>
+
+            <div class="btn-investir">
+                <input type="button" value="Investir">
+            </div>
+        </div>
+
+        <!-- Bolhas -->
+        <div class="circles">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <!-- Bolhas -->
+        <div class="container-2">
+            <img src="./img/cryptoPort.svg">
+        </div>
+    </div>
+
+    <!-- section 2 -->
+    <div class="section-2">
+        <div class="sec2-box-1">Portfólios inteligentes</div>
+        <div class="sec2-box-2">Com estratégias de investimento temáticas, através de uma exposição diversificada aos segmentos de mercado que moldarão as nossas vidas.</div>
+
+        <div class="logos">
+            <div class="logos-slide">
+                <img src="./img/bitcoin.jpg" alt="">
+                <img src="./img/ethereum.jpg" alt="">
+                <img src="./img/tether.jpg" alt="">
+                <img src="./img/xrp.jpg" alt="">
+                <img src="./img/bnb.jpg" alt="">
+                <img src="./img/dogecoin.jpg" alt="">
+            </div>
+            <div class="logos-slide">
+                <img src="./img/bitcoin.jpg" alt="">
+                <img src="./img/ethereum.jpg" alt="">
+                <img src="./img/tether.jpg" alt="">
+                <img src="./img/xrp.jpg" alt="">
+                <img src="./img/bnb.jpg" alt="">
+                <img src="./img/dogecoin.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+    <!-- section 3 -->
+
+    <div class="section-3">
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+        <i class="fa-brands fa-github"></i>
+    </div>
+
+    <!-- footer  -->
+
+</body>
+
+</html>
