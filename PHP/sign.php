be7f3b9281a0cccf856329dbfb809c1f8e6d96d1
Mateus Araujo <mateusaraujo3105@gmail.com>
2023-11-29 19:22:05 +0000

    Starting to work on form design

--- PHP/sign.php
+++ PHP/sign.php
@@ -15,9 +15,38 @@
                 <div class="logo-img"><img src="/img/bear.png"></div>
                 <div class="logo-name">BlueBerryBear</div>
             </div>
+            <hr>
+            <div class="box1-text">Estamos felizes por estar a juntar-se a nós!</div>
+            <div class="happy-img">
+                <img src="/img/undraw_happy_news_re_tsbd.svg">
+            </div>
         </div>
         <div class="box2">
-            box2
+            <div class="transparency-div">
+                <form action="#">
+                    <fieldset>
+                        <!-- Nome -->
+                        <label for="nome">Nome de utilizador</label>
+                        <input type="text" name="nome" id="nome" class="user">
+                        <!-- Email -->
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" class="user">
+                        <!-- Senha -->
+                        <label for="psw">Senha</label>
+                        <input type="password" class="user" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
+                        <!-- Telefone -->
+                        <label for="telefone">Telefone</label>
+                        <input type="tel" name="telefone" id="telefone" class="user" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" placeholder="000-000-000">
+                        <!-- Condições e Termos -->
+                        <label id="checkbox-lbl">
+                            <input type="checkbox" required checked>Eu concordo com as condições e os termos de uso.</label>
+                        <!-- Botao para enviar -->
+                        <div class="btn-submit">
+                            <input type="submit" value="Enviar">
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     </div>
 </body>
