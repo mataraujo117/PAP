11eb8d92555de4fc3032638de38425f9c6aecfd8
Mateus Araujo <mateusaraujo3105@gmail.com>
2023-11-29 18:17:13 +0000

    Starting to work on Sign up page Finishing Logo position and style

--- PHP/sign.php
+++ PHP/sign.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlueBerryBear</title>
+    <link rel="stylesheet" href="/CSS/sign.css">
 </head>
 
 <body>
     <div class="container">
         <div class="box1">
-            <div class="box1-igm"><img></div>
-            <div class="box1-text"></div>
+            <div class="logo">
+                <div class="logo-img"><img src="/img/bear.png"></div>
+                <div class="logo-name">BlueBerryBear</div>
+            </div>
         </div>
-        <div>
-
+        <div class="box2">
+            box2
         </div>
     </div>
 </body>
