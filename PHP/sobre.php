2661ff9144f76a1e3553559af7f924fb586efba3
Mateus Araujo <mateusaraujo3105@gmail.com>
2023-12-01 23:11:55 +0000

    I worked on the letters, to make it look more stylish

--- PHP/sobre.php
+++ PHP/sobre.php
@@ -31,8 +31,60 @@
 
     <div class="main">
 
-        <div class="container">
-            <div class="box">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed perspiciatis mollitia dignissimos voluptate assumenda! Accusantium impedit voluptas animi beatae laudantium non error modi explicabo incidunt dolorum cumque, blanditiis minima alias.</div>
+        <div class="container1">
+            <div class="box">
+                <span>Olá,</span>
+                <span>pessoal!</span>
+                <span>Eu</span>
+                <span>sou</span>
+                <span>o</span>
+                <span>Mateus,</span>
+                <span>um</span>
+                <span>jovem</span>
+                <span>de</span>
+                <span>18</span>
+                <span>anos</span>
+                <span>que</span>
+                <span>está</span>
+                <span>navegando</span>
+                <span>pela</span>
+                <span>incrível</span>
+                <span>jornada</span>
+                <span>do</span>
+                <span>ensino</span>
+                <span>médio</span>
+                <span>aqui</span>
+                <span>em</span>
+                <span>Palmela.</span>
+                <span>Desde</span>
+                <span>cedo,</span>
+                <span>descobri</span>
+                <span>minha</span>
+                <span>paixão</span>
+                <span>pelo</span>
+                <span>universo</span>
+                <span>da</span>
+                <span>programação,</span>
+                <span>e</span>
+                <span>hoje,</span>
+                <span>meu</span>
+                <span>coração</span>
+                <span>bate</span>
+                <span>mais</span>
+                <span>forte</span>
+                <span>pelo</span>
+                <span>fascinante</span>
+                <span>mundo</span>
+                <span>do</span>
+                <span>desenvolvimento</span>
+                <span>web.</span>
+            </div>
+        </div>
+
+        <div class="container2">
+            <div class="portrait">
+                <img src="/img/25d831231f60963683d818ace6571a30.jpg" alt="profile-picture">
+            </div>
         </div>
 
 
